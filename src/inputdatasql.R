5ac473f7d48c257394fa6f719ea812cc1ad76db8
Rahmi Anadra <127948357+rahmiandr@users.noreply.github.com>
2024-03-01 07:38:39 +0700

    Update inputdatasql.R

--- src/inputdatasql.R
+++ src/inputdatasql.R
@@ -186,36 +186,3 @@ result <- dbGetQuery(DB, "SELECT * FROM \"ulasan\"")
 ## Menampilkan hasil
 print(result)
 
----------------------------------------------------------------------
-  
-library(RPostgreSQL)
-library(DBI)
-driver <- dbDriver('PostgreSQL')
-DB <- dbConnect(
-  driver,
-  dbname="uafemknk", # User & Default database
-  host="topsy.db.elephantsql.com", # Server
-  # port=5432,
-  user="uafemknk", # User & Default database
-  password="Uxf_vl9JGfj4X283QHpW95GYSWD9DCbQ" # Password
-)
-
-
-dbhost<- dbConnect(
-  driver, 
-  dbname="novel_indo", 
-  host="localhost",
-  port=5432,
-  user="postgres",
-  password="1234567"
-)
-
-# select departemen dari table departemen
-penulis=dbReadTable(dbhost,'penulis')
-penerbit=dbReadTable(dbhost,'penerbit')
-novel=dbReadTable(dbhost,'novel')
-ulasan=dbReadTable(dbhost,'ulasan')
-dbWriteTable(DB,'penulis',penulis,overwrite=T,row.names=F)
-dbWriteTable(DB,'penerbit',penerbit,overwrite=T,row.names=F)
-dbWriteTable(DB,'novel',novel,overwrite=T,row.names=F)
-dbWriteTable(DB,'ulasan',ulasan,overwrite=T,row.names=F)
