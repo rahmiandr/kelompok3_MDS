0e31fb07add663feec88a82da9d039cfd75216fe
Rahmi Anadra <127948357+rahmiandr@users.noreply.github.com>
2024-03-01 07:58:24 +0700

    Add files via upload

---
+++ src/conn_elephantsql.R
@@ -0,0 +1,31 @@
+library(RPostgreSQL)
+library(DBI)
+driver <- dbDriver('PostgreSQL')
+DB <- dbConnect(
+  driver,
+  dbname="uafemknk", # User & Default database
+  host="topsy.db.elephantsql.com", # Server
+  # port=5432,
+  user="uafemknk", # User & Default database
+  password="Uxf_vl9JGfj4X283QHpW95GYSWD9DCbQ" # Password
+)
+
+
+dbhost<- dbConnect(
+  driver, 
+  dbname="novel_indo", 
+  host="localhost",
+  port=5432,
+  user="postgres",
+  password="1234567"
+)
+
+# select dari setiap tabel
+penulis=dbReadTable(dbhost,'penulis')
+penerbit=dbReadTable(dbhost,'penerbit')
+novel=dbReadTable(dbhost,'novel')
+ulasan=dbReadTable(dbhost,'ulasan')
+dbWriteTable(DB,'penulis',penulis,overwrite=T,row.names=F)
+dbWriteTable(DB,'penerbit',penerbit,overwrite=T,row.names=F)
+dbWriteTable(DB,'novel',novel,overwrite=T,row.names=F)
+dbWriteTable(DB,'ulasan',ulasan,overwrite=T,row.names=F)
